63392baf2f636058d618271f3844c3c1ad61dfa8
sobial <sobhaniali1@gmail.com>
2018-03-13 12:50:25 +0330

    basic file manager

--- test.py
+++ test.py
@@ -1,1 +1,32 @@
-print ("hello world")
+import os
+home = '/home/ali'
+currentdir = '/home/ali'
+
+
+
+#change directory
+def cd(filename):
+    global currentdir
+    currentdir = os.path.join(currentdir , filename)
+
+
+#lists files in curernt directory
+def ls ():
+    q = os.listdir(currentdir)
+    print(q)
+
+
+
+#basic file manager
+while True:
+    ls()
+    inp = input()
+
+    # .. means back !
+    if inp == '..':
+        currentdir = os.path.split(currentdir)[0]
+
+    else :
+        cd(os.listdir(currentdir)[int(inp)])
+
+
