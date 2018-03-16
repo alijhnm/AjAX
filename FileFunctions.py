07b6a6da4dac448a21a05773d01502f9b2da5540
alijhnm <35062650+alijhnm@users.noreply.github.com>
2018-03-16 11:29:35 +0330

    mkdir added, basic path exlporing added

--- FileFunctions.py
+++ FileFunctions.py
@@ -1,10 +1,10 @@
-import os, shutil
+import os, shutil, os.path
 
 def OpenFile(file_path):
     os.startfile(file_path)
 
 def CopyFile(file_path, target_path):
-    os.copy2(file_path, target_path)
+    shutil.copy2(file_path, target_path)
 
 def RenameFile(old_name, new_name):
     try:
@@ -30,10 +30,24 @@ def MakeZip(dir, target_path, name):
 def ExtractZip(archieve_path, extract_path):
     shutil.unpack_archive(archieve_path, extract_path)
 
-#ExtractZip('D:/testCompressed.zip', 'D:/')
-#MakeZip('D:/test-compress', 'D:/', 'testCompressed')
-#CopyFile('D:/Document/Untitled.png', 'D:/Aks.png')
-#RenameFile('D:/Server.bmp', 'D:/Siroos.bmp')
-#RemoveFile('D:/test.bmp')
-#RemoveDir('D:/test')
-#CutFile('D:/Server.py','D:/Git')
+def MakeDir(path = os.getcwd()):
+    os.mkdir(path)
+
+while True:
+    dir_info = next(os.walk(os.getcwd()))
+    print('current_path :', dir_info[0], '\navailable directories :', dir_info[1], '\navailable files :', dir_info[2])
+    command = input('Enter a number to enter a file(2) or dir(1), b to get to parrent dir, d to change drive :')
+    if command == 'b' or command == 'B':
+        back = os.path.dirname(os.getcwd())
+        os.chdir(back)
+    elif command[0] == 'd' or command[0] == 'D':
+        new_drive = command[1].capitalize() + ':/'
+        os.chdir(new_drive)
+    else:
+        new_path = dir_info[0] + '\\' + dir_info[int(command[0])][int(command[1:]) - 1]
+        if int(command[0]) == 1:
+            os.chdir(new_path)
+        elif int(command[0]) == 2:
+            OpenFile(new_path)
+        else:
+            print('Invalid entry')
